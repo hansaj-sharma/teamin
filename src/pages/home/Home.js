fd4bc9b679f497b54b7a8317d5c273db907e2971
Hansaj Sharma <hansajsharma123@hmail.com>
2022-06-10 23:04:18 +0530

    some changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,22 @@
 import './Home.css'
 
 import React from 'react'
-
-
+import Card from '../../components/Card'
+import CloudComputing from '../../assets/cloud_computing.svg'
+// const cardInfo = {
+//     cardImage: CloudComputing,
+//     cardTitle: "Web App",
+//     cardContent: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, quam."
+// }
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            {/* <div className='card-list'>
+                <Card cardInfo={cardInfo} />
+                <Card cardInfo={cardInfo} />
+                <Card cardInfo={cardInfo} />
+            </div> */}
         </div>
-
     )
 }

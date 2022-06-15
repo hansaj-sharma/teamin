e8a2b255477aa5bc8bbf65dae2db401aaaaab90d
Hansaj Sharma <hansajsharma123@hmail.com>
2022-06-16 00:33:30 +0530

    changes

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,53 +1,47 @@
 export const MenuItems = [
-    {
-       title: 'Home',
-       url: '#',
-       cName: 'nav-links'
+   {
+      title: 'Home',
+      url: '/',
+      cName: 'nav-links'
 
-    },
-    {
-        title: 'About Us',
-        url: '#',
-        cName:'nav-links'
- 
-     },
+   },
+   {
+      title: 'About Us',
+      url: '/about',
+      cName: 'nav-links'
 
-    {
-        title: 'Services',
-        url: '#',
-        cName:'nav-links'
- 
-     },
+   },
 
 
-     {
-        title: 'Products',
-        url: '#',
-        cName:'nav-links'
- 
-     },
 
-     {
-        title: 'Clients',
-        url: '#',
-        cName:'nav-links'
- 
-     },
- 
-     {
-        title: 'Career',
-        url: '#',
-        cName:'nav-links'
- 
-     },
- 
-     {
-        title: 'Contact Us',
-        url: '#',
-        cName :'nav-links'
- 
-     }
- 
-  
+   {
+      title: 'Products',
+      url: '/products',
+      cName: 'nav-links'
+
+   },
+
+   {
+      title: 'Clients',
+      url: '/clients',
+      cName: 'nav-links'
+
+   },
+
+   {
+      title: 'Career',
+      url: '/career',
+      cName: 'nav-links'
+
+   },
+
+   {
+      title: 'Contact Us',
+      url: '/contact',
+      cName: 'nav-links'
+
+   }
+
+
 
 ]
